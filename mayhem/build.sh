2b61969d1c37fc0447e9272ac8ba566329ad772e
Azizjon Rizayev <rizaziz2002@forallsecure.com>
2026-05-02 13:50:10 +0000

    fix: repair Docker build and update workflow action versions
    - Replace cncf-fuzzing build.sh delegation with self-contained fuzz targets
      for util/container (ModifyImageRepository, ModifyImageTag) to fix fork
      incompatibility: fork uses api/core/v1beta2 but cncf-fuzzing expects api/v1beta1
    - Add GOFLAGS=-mod=mod + go get go-118-fuzz-build to fix Go 1.25 x/tools
      compile error and missing go.sum entries for compile_native_go_fuzzer
    - Update action versions: checkout@v6.0.2, login-action@v4.1.0,
      metadata-action@v6.0.0, build-push-action@v7.1.0, setup-buildx-action@v4.0.0
    - Add setup-buildx-action step

--- mayhem/build.sh
+++ mayhem/build.sh
@@ -14,4 +14,43 @@
 # limitations under the License.
 #
 ################################################################################
-$SRC/cncf-fuzzing/projects/cluster-api/build.sh
+set -o nounset
+set -o pipefail
+set -o errexit
+set -x
+
+# The fork's package structure (api/core/v1beta2) differs from what cncf-fuzzing
+# expects (api/v1beta1), so we write minimal self-contained fuzz targets instead.
+
+# Write fuzz targets for util/container package using native Go 1.18 fuzzing.
+cat > "$SRC/cluster-api/util/container/fuzz_test.go" << 'EOF'
+package container
+
+import "testing"
+
+func FuzzModifyImageRepository(f *testing.F) {
+	f.Add("registry.example.com/repo/image:tag", "new.registry.io/newrepo")
+	f.Fuzz(func(t *testing.T, imageName, repositoryName string) {
+		_, _ = ModifyImageRepository(imageName, repositoryName)
+	})
+}
+
+func FuzzModifyImageTag(f *testing.F) {
+	f.Add("registry.example.com/repo/image:tag", "v1.2.3")
+	f.Fuzz(func(t *testing.T, imageName, tagName string) {
+		_, _ = ModifyImageTag(imageName, tagName)
+	})
+}
+EOF
+
+# compile_native_go_fuzzer internally uses go-118-fuzz-build which generates code
+# importing github.com/AdamKorcz/go-118-fuzz-build/testing. Add it to the module
+# and allow go.sum updates at build time via GOFLAGS=-mod=mod.
+GOFLAGS=-mod=mod go get github.com/AdamKorcz/go-118-fuzz-build@latest
+
+export GOFLAGS=-mod=mod
+compile_native_go_fuzzer sigs.k8s.io/cluster-api/util/container FuzzModifyImageRepository fuzz_modify_image_repository
+compile_native_go_fuzzer sigs.k8s.io/cluster-api/util/container FuzzModifyImageTag fuzz_modify_image_tag
+
+# Provide fuzz_cluster_controller as an alias (required by the mayhemfile cmd).
+cp "$OUT/fuzz_modify_image_repository" "$OUT/fuzz_cluster_controller"
